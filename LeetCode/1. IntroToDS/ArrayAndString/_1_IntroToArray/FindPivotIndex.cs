dcbe6d9562dbb8c0d0f5e1e227ed3ae4b2c0bb0b
jeffreyh93 <43526518+jeffreyh93@users.noreply.github.com>
2022-04-20 19:32:59 -0700

    Finished 1.1.1 - Find Pivot Index

---
+++ LeetCode/1. IntroToDS/ArrayAndString/_1_IntroToArray/FindPivotIndex.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode._1._IntroToDS.ArrayAndString._1_IntroToArray
+{
+    public static class FindPivotIndex
+    {
+        // Given an array of integers, nums, calculate the pivot index of the array
+        // The pivot index is the index where the sum of all the numbers strictly to the left of the index is equal to the sum
+        //      of all the numbers strictly to the index's right
+        // If the index is on the left edge of the array, then the left sum is 0 because there are no elements left. This also
+        //      applies to the right edge of the array
+        // Return the leftmost pivot index. If no such index exists, return -1
+
+        // Input: nums = [1,7,3,6,5,6], output: 3
+        // Input: nums = [1,2,3], output: -1
+        // Input: nums = [2,1,-1], output: 0
+
+        public static int PivotIndex(int[] nums) {
+            int sum = 0;
+            foreach (int n in nums) sum += n;
+
+            int leftSum = 0;
+            int rightSum = sum;
+
+            for (int i = 0; i < nums.Length; i++) {
+                if (i != 0) leftSum += nums[i - 1];
+                rightSum -= nums[i];
+                if (leftSum == rightSum) return i;
+            }
+            return -1;
+        }
+    }
+}
--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -1,15 +1,47 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+using LeetCode._1._IntroToDS.ArrayAndString._1_IntroToArray;
 
 namespace LeetCode
 {
     class Program
     {
+        private static string PrintArr(int[] arr) {
+            string ret = "";
+            foreach (int n in arr) {
+                ret += n + ",";
+            }
+            return ret;
+        }
         // driver for all of the sections
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
 
             // Intro to Data Structures
+            Console.WriteLine("1. Introduction to Data Structures");
+
+            // Arrays and Strings
+            Console.WriteLine("Arrays and Strings");
+
+            // Introduction to Array
+            Console.WriteLine("Introduction to Array");
+
+            // 1. Find Pivot Index
+            Console.WriteLine("1a. Find Pivot Index");
+            {
+                List<int[]> numsArr = new List<int[]>();
+                numsArr.Add(new int[] { 1, 7, 3, 6, 5, 6 });
+                numsArr.Add(new int[] { 1, 2, 3 });
+                numsArr.Add(new int[] { 2, 1, -1 });
+
+                foreach (int[] arr in numsArr)
+                {
+                    Console.WriteLine($"arr = {PrintArr(arr)} result = {FindPivotIndex.PivotIndex(arr)}");
+                }
+            }
 
             // Into to Algorithms
 

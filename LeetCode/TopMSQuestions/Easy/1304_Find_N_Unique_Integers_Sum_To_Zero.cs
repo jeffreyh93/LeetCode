2d7bef8a33f86c481ebd347e8d4593d2fcc505ec
jeffreyh93 <43526518+jeffreyh93@users.noreply.github.com>
2022-06-22 22:04:06 -0700

    Finished MS Easy Questions

--- LeetCode/TopMSQuestions/Easy/1304_Find_N_Unique_Integers_Sum_To_Zero.cs
+++ LeetCode/TopMSQuestions/Easy/1304_Find_N_Unique_Integers_Sum_To_Zero.cs
@@ -8,5 +8,18 @@ namespace LeetCode.TopMSQuestions.Easy
 {
     class _1304_Find_N_Unique_Integers_Sum_To_Zero
     {
+        /**
+         * Algorithm: Fill the array from both sides
+         *      - ie. if n = 5, fill -2, -1, 0, 1, 2
+         *      - meaning each index = -(n / 2 - i), n / 2 - i
+         */ 
+        public int[] SumZero(int n) {
+            int[] ret = new int[n];
+            for (int i = 0; i < n / 2; i++) {
+                ret[i] = -(n / 2 - i);
+                ret[n - i - 1] = n / 2 - i;
+            }
+            return ret;
+        }
     }
 }
--- LeetCode/TopMSQuestions/Easy/1822_Sign_Of_Product_Of_Array.cs
+++ LeetCode/TopMSQuestions/Easy/1822_Sign_Of_Product_Of_Array.cs
@@ -8,5 +8,19 @@ namespace LeetCode.TopMSQuestions.Easy
 {
     class _1822_Sign_Of_Product_Of_Array
     {
+        /**
+         * Algorithm:
+         *      - have a variable that tracks the sign
+         *      - everytime we come across a negative number, flip the variable
+         *      - if we come across a 0, assign that
+         */ 
+        public int ArraySign(int[] nums) {
+            int sign = 1;
+            foreach (int n in nums) {
+                if (n == 0) return 0;
+                if (n < 0) sign *= -1;
+            }
+            return sign;
+        }
     }
 }
